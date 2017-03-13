e98d4981ed8dc0351e3acd30ea3a9226b3925ea7
Dylan Mouthaan <info@dylanmouthaan.nl>
2017-03-13 15:19:15 +0100

    Man

---
+++ man.js
@@ -0,0 +1,4 @@
+if(window.dataLayer[0].customerGender === "man") {
+      $('div.eight.columns > a:nth-child(1) > img').attr('src', 'https://assets.wehkamp.com/i/wehkamp/heren-blok4-wk10.jpg');
+   }else {
+      }
